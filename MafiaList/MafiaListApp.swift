7290d05118328788c39f6204d68555734eaa77b8
Evandruce Phincon <evandruce.filbert@phincon.com>
2023-08-14 19:55:27 +0700

    Initial Commit

---
+++ MafiaList/MafiaListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MafiaListApp.swift
+//  MafiaList
+//
+//  Created by Phincon on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MafiaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
